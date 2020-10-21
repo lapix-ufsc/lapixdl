ec4feb5093cb7248512567e30ce685c69ad13090
André Victória Matias <andre.vmatias@gmail.com>
2020-10-21 10:44:36 -0300

    Correção de homepage do projeto

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/johnnv1/lapix",
+    url="https://github.com/lapix-ufsc/lapixdl",
 
     author='LAPIX',
     license='MIT',
