154937d8dd4f0c85ff4e13c8071b7e54828ddd4e
André Victória Matias <andre.vmatias@gmail.com>
2020-10-29 20:44:22 -0300

    addition of examples.py

---
+++ examples.py
@@ -0,0 +1,25 @@
+"""
+Module use examples
+"""
+
+
+def main():
+    # Model evaluation examples
+    evaluate_segmentation_example()
+    evaluate_detection_example()
+    evaluate_classification_example()
+
+
+def evaluate_segmentation_example():
+    pass
+
+
+def evaluate_detection_example():
+    pass
+
+
+def evaluate_classification_example():
+    pass
+
+
+main()
