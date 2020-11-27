e594f908af6a21c401238ab2511995d46a96d092
André Victória Matias <andre.vmatias@gmail.com>
2020-11-27 10:55:30 -0300

    Adição de testes extremos de iou

--- tests/evaluation/evaluate/test_calculate_pairwise_bbox_ious.py
+++ tests/evaluation/evaluate/test_calculate_pairwise_bbox_ious.py
@@ -12,6 +12,8 @@ def test_calculate_pairwise_bbox_ious():
         BBox(31, 69, 201-31+1, 125-69+1, 0),
         BBox(50, 72, 197-50+1, 121-72+1, 0),
         BBox(35, 51, 196-35+1, 110-51+1, 0),
+        BBox(35, 51, 196, 110, 0),
+        BBox(0, 0, 5, 5, 0),
     ]
     pred_bboxes = [
         BBox(54, 66, 198-54+1, 114-66+1, 0),
@@ -19,12 +21,17 @@ def test_calculate_pairwise_bbox_ious():
         BBox(18, 63, 235-18+1, 135-63+1, 0),
         BBox(54, 72, 198-54+1, 120-72+1, 0),
         BBox(36, 60, 180-36+1, 108-60+1, 0),
+        BBox(35, 51, 196, 110, 0),
+        BBox(6, 6, 5, 5, 0),
     ]
 
     ious = calculate_pairwise_bbox_ious(gt_bboxes, pred_bboxes)
 
     assert ious[1, 1] == 0
+
+    assert round(ious[0, 0], 3) == .798
+    assert round(ious[2, 2], 3) == .612
     assert round(ious[3, 3], 3) == .947
     assert round(ious[4, 4], 3) == .731
-    assert round(ious[2, 2], 3) == .612
-    assert round(ious[0, 0], 3) == .798
+    assert ious[5, 5] == 1
+    assert ious[6, 6] == 0
