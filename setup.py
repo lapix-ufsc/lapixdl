7ec7a573fbdb653c43e6c8cc867d5ca3811f5c25
André Victória Matias <andre.vmatias@gmail.com>
2021-03-01 11:36:27 -0300

    Ajuste de versão

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name='lapixdl',
     packages=find_packages(exclude=['tests']),
-    version='0.7',
+    version='0.6.1',
     description='Utils for Computer Vision Deep Learning research',
 
     long_description=long_description,
