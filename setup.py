e8481e07b378e7ac1f3db095a61ad590fca85bc9
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2022-02-03 16:11:23 -0300

    Update version to 0.8.11

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name='lapixdl',
     packages=find_packages(exclude=['tests']),
-    version='0.8.01',
+    version='0.8.11',
     description='Utils for Computer Vision Deep Learning research',
 
     long_description=long_description,
